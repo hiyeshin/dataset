4ea79669affd37e73fcf83732b7cfe4dc2d6b89b
Hiye Shin <hiyeshin@gmail.com>
2012-11-07 14:00:30 -0500

    api made

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import os, datetime
 import re
+import requests
 from unidecode import unidecode
 
-from flask import Flask, request, render_template, redirect, abort
+from flask import Flask, request, render_template, redirect, abort, jsonify
 
 # import all of mongoengine
 from mongoengine import *
@@ -20,8 +21,6 @@ connect('mydata', host=os.environ.get('MONGOLAB_URI'))
 app.logger.debug("Connecting to MongoLabs")
 
 # hardcoded categories for the checkboxes on the form
-#categories = ['web','physical computing','software','video','music','installation','assistive technology','developing nations','business','social networks']
-
 # --------- Routes ----------
 
 # this is our main page
@@ -77,6 +76,80 @@ def thanks():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+
+
+
+
+@app.route('/data/demographics')
+def data_demographics():
+
+	# query for the ideas - return oldest first, limit 10
+	users = models.User.objects().order_by('+timestamp')
+
+	if users:
+
+		# list to hold ideas
+		public_users = []
+
+		#prep data for json
+		for i in users:
+			
+			tmpUser = {
+				'user_name' : i.name,
+				'longest_residence' : i.longest_residence,
+				'birthplace' : i.birthplace,
+				'language': i.language,
+				'countries_lived': i.countries_lived,
+				'citizenship': i.citizenship,
+				'timestamp' : str( i.timestamp )
+			}
+
+
+
+
+	# name = StringField(max_length=120, required=True, verbose_name="What's your name?")
+	# birthplace = StringField(max_length=120, required=True, verbose_name = "In what country were you born?")
+	# citizenship = StringField(required = True, verbose_name = "Where do you hold your citizenship?")
+	# longest_residence = StringField(required=True, verbose_name="What country have you lived in longest?")
+	# language = StringField(required=True, verbose_name="What languages can you speak?")
+	# countries_lived = StringField(required=True, verbose_name = "In what countries have you lived?")
+	# # Timestamp will record the date and time idea was created.
+	# timestamp = DateTimeField(default=datetime.now())
+
+			# comments / our embedded documents
+			#tmpIdea['comments'] = [] # list - will hold all comment dictionaries
+			
+			# loop through idea comments
+			# for c in i.comments:
+			# 	comment_dict = {
+			# 		'name' : c.name,
+			# 		'comment' : c.comment,
+			# 		'timestamp' : str( c.timestamp )
+			# 	}
+
+			# 	# append comment_dict to ['comments']
+			# 	tmpIdea['comments'].append(comment_dict)
+
+			# # insert idea dictionary into public_ideas list
+			public_users.append( tmpUser )
+
+		# prepare dictionary for JSON return
+		data = {
+			'status' : 'OK',
+			'users' : public_users
+		}
+
+		# jsonify (imported from Flask above)
+		# will convert 'data' dictionary and set mime type to 'application/json'
+		return jsonify(data)
+
+	else:
+		error = {
+			'status' : 'error',
+			'msg' : 'unable to retrieve ideas'
+		}
+		return jsonify(error)
 
 
 # slugify the title 
